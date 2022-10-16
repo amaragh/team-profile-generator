1df2a1717cadf091025ce35e291956ef3122c25d
amaragh <mercer.alecia@gmail.com>
2022-10-16 16:59:43 -0500

    update employee tests for employee ID to be numeric instead of string

--- __tests__/Employee.tests.js
+++ __tests__/Employee.tests.js
@@ -1,16 +1,16 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Deb','1234','deb@test.com');
+    const employee = new Employee('Deb',1234,'deb@test.com');
 
     expect(employee.name).toBe('Deb');
-    expect(employee.id).toBe('1234');
+    expect(employee.id).toBe(1234);
     expect(employee.email).toBe('deb@test.com');
 
 });
 
 test("get employee's name",() => {
-    const employee = new Employee('Deb','1234','deb@test.com');
+    const employee = new Employee('Deb',1234,'deb@test.com');
 
     const actual = employee.getName();
     const expected = 'Deb';
@@ -20,17 +20,17 @@ test("get employee's name",() => {
 });
 
 test("get employee's id",() => {
-    const employee = new Employee('Deb','1234','deb@test.com');
+    const employee = new Employee('Deb',1234,'deb@test.com');
 
     const actual = employee.getId();
-    const expected = '1234';
+    const expected = 1234;
 
     expect(actual).toBe(expected);
 
 });
 
 test("get employee's Email",() => {
-    const employee = new Employee('Deb','1234','deb@test.com');
+    const employee = new Employee('Deb',1234,'deb@test.com');
 
     const actual = employee.getEmail();
     const expected = 'deb@test.com';
@@ -41,7 +41,7 @@ test("get employee's Email",() => {
 
 
 test("get employee's role",() => {
-    const employee = new Employee('Deb','1234','deb@test.com');
+    const employee = new Employee('Deb',1234,'deb@test.com');
 
     const actual = employee.getRole();
     const expected = 'Employee';
