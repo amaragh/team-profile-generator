4da13d24196d45cabc50950479955752367ec2d9
amaragh <mercer.alecia@gmail.com>
2022-10-17 20:09:01 -0500

    update prompt functions to gather all employee data

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template')
 
 const promptManager = () => {
 
@@ -8,102 +10,169 @@ const promptManager = () => {
      (all prompts are required)
     ============================
     `);
-    return inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'mgrName',
-                message: 'What is your name?',
-                validate: mgrNameInput => {
-                    if (mgrNameInput) {
-                        return true
-                    } else {
-                        console.log("Please enter your name!");
-                        return false;
-                    }
-                }
-            }, {
-                type: 'input',
-                name: 'mgrId',
-                message: 'What is your Employee ID?',
-                validate: mgrIdInput => {
-                    if (mgrIdInput) {
-                        return true
-                    } else {
-                        console.log("Please enter your Employee ID!");
-                        return false;
-                    }
-                }
-            }, {
-                type: 'input',
-                name: 'mgrEmail',
-                message: 'What is your email address?',
-                validate: mgrEmailInput => {
-                    if (mgrEmailInput) {
-                        return true
-                    } else {
-                        console.log("Please enter your email address!");
-                        return false;
-                    }
-                }
-            }, {
-                type: 'input',
-                name: 'officeNum',
-                message: 'What is your office number?',
-                validate: officeNumInput => {
-                    if (officeNumInput) {
-                        return true
-                    } else {
-                        console.log("Please enter your office number!");
-                        return false;
-                    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'mgrName',
+            message: 'What is your name?',
+            validate: mgrNameInput => {
+                if (mgrNameInput) {
+                    return true
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
                 }
             }
+        }, {
+            type: 'input',
+            name: 'mgrId',
+            message: 'What is your Employee ID?',
+            validate: mgrIdInput => {
+                if (mgrIdInput) {
+                    return true
+                } else {
+                    console.log("Please enter your Employee ID!");
+                    return false;
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'mgrEmail',
+            message: 'What is your email address?',
+            validate: mgrEmailInput => {
+                if (mgrEmailInput) {
+                    return true
+                } else {
+                    console.log("Please enter your email address!");
+                    return false;
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'officeNum',
+            message: 'What is your office number?',
+            validate: officeNumInput => {
+                if (officeNumInput) {
+                    return true
+                } else {
+                    console.log("Please enter your office number!");
+                    return false;
+                }
+            }
+        }
 
-        ])
+    ])
 };
 
-const promptEmployee = () => {
+const promptEmployee = staffingData => {
 
-    return inquirer
-        .prompt([{
+
+
+    if (!staffingData.employees) {
+        staffingData.employees = [];
+    };
+
+    return inquirer.prompt([
+        {
             type: 'list',
             name: 'addOrExit',
             message: 'Would you like to add an employee or finish building your team?',
             choices: ['Add Engineer', 'Add Intern', 'Finish building team']
         }
-            ,
+        ,
         {
             type: 'input',
-            name: 'engName',
-            message: 'Engineer Name?',
+            name: 'empName',
+            message: "What is the employee's name?",
             when: (answers) => {
-                if (answers.addOrExit === 'Add Engineer') {
-                    return true;
-                } else {
-                    return false;
-                }
+                return answers.addOrExit !== 'Finish building team';
             }
-        }
-            ,
+        },
         {
             type: 'input',
-            name: 'intName',
-            message: 'Intern Name?',
-            when: ( answers ) => {
-                if (answers.addOrExit === 'Add Intern') {
-                    return true;
-                } else {
-                    return false;
-                }
+            name: 'empId',
+            message: "What is the employee's ID?",
+            when: (answers) => {
+                return answers.addOrExit !== 'Finish building team';
+            }
+        },
+        {
+            type: 'input',
+            name: 'empEmail',
+            message: "What is the employee's email address?",
+            when: (answers) => {
+                return answers.addOrExit !== 'Finish building team';
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'empGithub',
+            message: "What is the employee's GitHub username?",
+            when: (answers) => {
+                return answers.addOrExit === 'Add Engineer';
+            }
+        },
+        {
+            type: 'input',
+            name: 'empSchool',
+            message: "What is the name of the Intern's school?",
+            when: (answers) => {
+                return answers.addOrExit === 'Add Intern';
             }
         }
-
-        ])
+    ]).then(empData => {
+        staffingData.employees.push(empData);
+        if (empData.addOrExit !== 'Finish building team') {
+            return promptEmployee(staffingData);
+        } else {
+            return staffingData;
+        }
+    })
 
 };
 
+// TODO: Create a function to write HTML file
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(`./dist/${fileName}`, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
 
+        })
+    })
+}
 
 promptManager()
     .then(promptEmployee)
+    .then(responseData => {
+        console.log(responseData)
+        return generatePage(responseData);
+    })
+    .then(writeHtmlFile => {
+        console.log("Your HTML file has been created")
+
+        return writeToFile('page.html', writeHtmlFile)
+    })
+
+    // promptManager()
+    // .then(response => {
+    //     console.log(response)
+    //     responses.push(response);
+    // })
+    // .then(promptEmployee)
+    // .then(answers => {
+    //     responses.push(answers)
+    //     console.log(responses)
+    // })
+    // .then(responseData => {
+    //     return generatePage(responseData);
+    // })
+    // .then(writeHtmlFile => {
+    //     console.log("Your HTML file has been created")
+
+    //     return writeToFile('page.html', writeHtmlFile)
+    // })
+
